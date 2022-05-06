96a6fd17c63c5d5f18dcec5c97ab0f1b251d429d
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-05-06 16:22:28 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react';
 import '../styles/App.css';
 const App = () => {
-
+  setInterval(updateTime,1000);
+  let [time,setTime]=useState(new Date().toLocaleTimeString());
+  function updateTime(){
+    setTime(new Date().toLocaleTimeString());
+  }
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{time}</div>
     </div>
   )
 }
